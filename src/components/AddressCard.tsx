d4388e05fffc6e0f0c3393bc78760d4f7bed3d71
Amnon Manneberg <148687878+Amnonman@users.noreply.github.com>
2025-12-07 21:31:25 +0200

    v0.0.23 - Debug: Inline SVG for Edit button

--- src/components/AddressCard.tsx
+++ src/components/AddressCard.tsx
@@ -95,7 +95,21 @@ export const AddressCard = ({
                 }}
                 aria-label="Edit address"
               >
-                <Pencil className="h-4 w-4" />
+                {/* Inline SVG debug test */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="h-4 w-4"
+                >
+                  <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z" />
+                </svg>
               </button>
             </TooltipTrigger>
             <TooltipContent className="tooltip-content">
