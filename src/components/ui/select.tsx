9bceeb0be92d82563a1abf04883a00eea0035815
Amnon Manneberg <148687878+Amnonman@users.noreply.github.com>
2025-12-08 22:21:56 +0200

    v0.0.77: Disable portal for Select inside Dialog
    - Added usePortal prop to SelectContent component
    - Set usePortal={false} for country/state selects in edit modal
    - Completely eliminates portal conflict with Dialog
    - Fixes removeChild DOM error when clicking dropdowns

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -58,11 +58,15 @@ const SelectScrollDownButton = React.forwardRef<
 ));
 SelectScrollDownButton.displayName = SelectPrimitive.ScrollDownButton.displayName;
 
+interface SelectContentProps extends React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content> {
+  usePortal?: boolean;
+}
+
 const SelectContent = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }, ref) => (
-  <SelectPrimitive.Portal>
+  SelectContentProps
+>(({ className, children, position = "popper", usePortal = true, ...props }, ref) => {
+  const content = (
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
@@ -86,8 +90,14 @@ const SelectContent = React.forwardRef<
       </SelectPrimitive.Viewport>
       <SelectScrollDownButton />
     </SelectPrimitive.Content>
-  </SelectPrimitive.Portal>
-));
+  );
+
+  if (usePortal) {
+    return <SelectPrimitive.Portal>{content}</SelectPrimitive.Portal>;
+  }
+  
+  return content;
+});
 SelectContent.displayName = SelectPrimitive.Content.displayName;
 
 const SelectLabel = React.forwardRef<
