1968af6eabb6e9573f855814521327a94c8b8f05
Amnon Manneberg <148687878+Amnonman@users.noreply.github.com>
2025-12-10 13:15:35 +0200

    v0.1.3: Fix default star button - keep highlight on click, disable pointer events

--- hp-react-widgets.php
+++ hp-react-widgets.php
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('HP_RW_VERSION', '0.1.2');
+define('HP_RW_VERSION', '0.1.3');
 define('HP_RW_FILE', __FILE__);
 define('HP_RW_PATH', plugin_dir_path(__FILE__));
 define('HP_RW_URL', plugin_dir_url(__FILE__));
